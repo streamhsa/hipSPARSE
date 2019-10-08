356f92f4a648925489dd97ae6ab438cab1f6b480
Pruthvi <pmagundu@amd.com>
2019-10-07 17:00:56 -0700

    hipSPARSE - Changes for relocatable installation  - Updated the lib so version, so name spec and RPATH.  - Changes are done as part of the experiment for FBA-29/69
    Signed-off-by: Pruthvi <pmagundu@amd.com>

--- install.sh
+++ install.sh
@@ -113,7 +113,7 @@ install_packages( )
   fi
 
   # dependencies needed for library and clients to build
-  local library_dependencies_ubuntu=( "make" "cmake-curses-gui" "hip_hcc" "pkg-config" "libnuma1" )
+  local library_dependencies_ubuntu=( "make" "cmake" "hip_hcc" "pkg-config" "libnuma1" )
   local library_dependencies_centos=( "epel-release" "make" "cmake3" "hip_hcc" "gcc-c++" "rpm-build" "numactl-libs" )
   local library_dependencies_fedora=( "make" "cmake" "hip_hcc" "gcc-c++" "libcxx-devel" "rpm-build" "numactl-libs" )
 
@@ -290,7 +290,7 @@ fi
 
 # We append customary rocm path; if user provides custom rocm path in ${path}, our
 # hard-coded path has lesser priority
-export PATH=${PATH}:/opt/rocm/bin
+export PATH=${PATH}:/opt/rocm-2.8.1/bin
 
 pushd .
   # #################################################
@@ -314,7 +314,7 @@ pushd .
   fi
 
   # cpack
-  cmake_common_options="${cmake_common_options} -DCPACK_SET_DESTDIR=OFF -DCPACK_PACKAGING_INSTALL_PREFIX=/opt/rocm"
+  cmake_common_options="${cmake_common_options} -DCPACK_SET_DESTDIR=OFF -DCPACK_PACKAGING_INSTALL_PREFIX=/opt/rocm-2.8.1"
 
   # cuda or hip
   if [[ "${build_cuda}" == false ]]; then
